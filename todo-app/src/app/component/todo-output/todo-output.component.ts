3c259b4608790567f879203f44edc73b7a018581
sajith udara perera <001sajith@gmail.com>
2021-09-25 22:15:13 +0530

    feat: CSS knowladge (add css class, how to chaneg css class using property binding)

--- todo-app/src/app/component/todo-output/todo-output.component.ts
+++ todo-app/src/app/component/todo-output/todo-output.component.ts
@@ -12,15 +12,35 @@ export class TodoOutputComponent implements OnInit {
 
   // children to parent data pass
   @Output() delete = new EventEmitter();
+
+  styleDiv = 'output-s1';
+  styleButton = 'style-button-s1'
   constructor() { }
 
   ngOnInit() {
   }
 
-  
+
   onDelete(selected)
   {
     // children to parent data pass
     this.delete.emit(selected);
+  }
+
+
+  // style change class (we can bind html element style)
+  onchange()
+  {
+    if(this.styleDiv == 'output-s2')
+    {
+      this.styleDiv = 'output-s1'
+      this.styleButton = 'style-button-s1'
+    }
+    else
+    {
+      this.styleDiv = 'output-s2'
+      this.styleButton = 'style-button-s2'
+    }
+    
   }
 }
